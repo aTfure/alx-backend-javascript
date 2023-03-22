261ea859cd614db16b4343c36c33b7aa84b96461
aTfure <atfure@gmail.com>
2023-03-22 22:47:38 +0000

    resolve

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,9 @@
+/* eslint-disable */
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName });
+  return new Promise(resolve => {
+    resolve({
+      firstName: firstName,
+      lastName: lastName,
+    })
+  })
 }
